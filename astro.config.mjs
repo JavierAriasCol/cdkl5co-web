3fb7c2dd7f9124e7d5b385d6e8179fdb50c5a7e6
Javier Arias <103268935+JavierAriasCol@users.noreply.github.com>
2026-04-01 13:30:50 -0500

    backup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import storyblok from '@storyblok/astro';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+	output: 'server',
+	adapter: cloudflare(),
+	integrations: [
+		storyblok({
+			accessToken: import.meta.env.STORYBLOK_TOKEN || process.env.STORYBLOK_TOKEN,
+			components: {
+				miembro: 'storyblok/Miembro',
+				evento: 'storyblok/Evento',
+			},
+			apiOptions: {
+				region: 'us',
+			},
+		}),
+	],
 });
